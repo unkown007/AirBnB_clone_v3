24039207ca814956766e17f4a9686957e10706cc
unkown007 <victormatusse00@gmail.com>
2023-07-31 15:57:49 +0200

    added file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """ setup routes """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+hbnbData = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User",
+        }
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """ return the status of the api """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """ returns the statistics of the data """
+    dict_stat = {}
+    for key, value in hbnbData.items():
+        dict_stat[key] = storage.count(value)
+    return jsonify(dict_stat)
