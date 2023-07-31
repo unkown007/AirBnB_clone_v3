ac0f6dcad2a024d8b7f8da66db4e5ef4ca6e0456
unkown007 <victormatusse00@gmail.com>
2023-07-31 16:56:38 +0200

    added file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ setup a Flask app """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def closeSession(error):
     """ close the current SQLAlchemy section """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_api_error(ex):
+    """ handle 404 not found error """
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":
