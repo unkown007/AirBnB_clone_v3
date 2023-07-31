ab3c4d8619ef40b6dff763cf8272501b1cf1b1b4
unkown007 <victormatusse00@gmail.com>
2023-07-31 16:59:33 +0200

    addded file

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,7 +19,7 @@ def closeSession(error):
 @app.errorhandler(404)
 def handle_api_error(ex):
     """ handle 404 not found error """
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
